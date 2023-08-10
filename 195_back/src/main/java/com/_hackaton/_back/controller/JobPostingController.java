38d63312763b4848407210e405a9b2cd8434eadf
jjw0071 <jjw0071@naver.com>
2023-08-11 00:04:18 +0900

    feat: JobPosting 컨트롤러 엔드포인트 업데이트
    - 구직 공고 생성을 위한 엔드포인트를 "/create_job_posting"으로 변경
    - 모든 구직 공고 조회를 위한 엔드포인트를 "/job_postings"으로 수정
    
    --- COMMIT END ---

--- 195_back/src/main/java/com/_hackaton/_back/controller/JobPostingController.java
+++ 195_back/src/main/java/com/_hackaton/_back/controller/JobPostingController.java
@@ -28,7 +28,7 @@ public class JobPostingController {
      * @return 생성된 구직 공고의 정보와 함께 상태 코드 200을 반환. 실패 시 에러 메시지와 함께 다른 상태 코드를 반환.
      * @throws IOException 구직 공고와 관련된 파일들을 처리하면서 발생할 수 있는 입출력 예외
      */
-    @PostMapping(value = "/create_job_postings", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/create_job_posting", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<JobPosting> createJobPosting(
             @ModelAttribute JobPostingRequestBasicInfoDto basicInfoDto,
             @ModelAttribute JobPostingRequestFileDto fileDto) throws IOException {
@@ -55,7 +55,7 @@ public class JobPostingController {
      *
      * @return 조회된 JobPosting 객체 리스트의 ResponseEntity
      */
-    @GetMapping(value = "/all_job_postings")
+    @GetMapping(value = "/job_postings")
     public ResponseEntity<List<JobPosting>> getAllJobPostings() {
         List<JobPosting> jobPostings = jobPostingService.getAllJobPostings();
         return ResponseEntity.ok(jobPostings);
